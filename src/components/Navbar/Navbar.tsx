40f74cf936b54b49e05ceedb3364c3788cf2329d
IPVM7 <ianvaldebenitom@gmail.com>
2023-03-07 21:26:30 -0300

    Added Navbar and components layout

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Flex, Image } from "@chakra-ui/react";
+import SearchInput from './SearchInput';
+
+
+const Navbar:React.FC = () => {
+    
+    return(
+        <Flex bg="white" height='44px' padding='6px 12px'>
+            <Flex align="center">
+                <Image src="/images/redditFace.svg" height='30px' alt='Reddit Icon'/>
+                <Image
+                 src="/images/redditText.svg" 
+                 height='46px' 
+                 display={{ base: "none" , md: "unset" }}
+                 alt='Text reddit' 
+                />
+            </Flex>
+            <SearchInput/>
+            {/*
+            <Directory/>
+            <RightContent/>*/}
+        </Flex>
+    );
+};
+export default Navbar;
