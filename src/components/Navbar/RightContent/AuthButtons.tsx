81a57b3cdb6961287a5b3792cbab9175f2e1bc1f
IPVM7 <ianvaldebenitom@gmail.com>
2023-03-09 16:14:14 -0300

    Added Login and SignUp Buttons

--- src/components/Navbar/RightContent/AuthButtons.tsx
+++ src/components/Navbar/RightContent/AuthButtons.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import { Button } from "@chakra-ui/react";
+import { authModalState } from 'Hypnos/atoms/authModalAtom';
+import { useSetRecoilState } from 'recoil'
 
 const AuthButtons:React.FC = () => {
-    
+    const setAuthModalState = useSetRecoilState(authModalState);
     return (
         <>
-        <Button>Log In</Button>
-        <Button>Sign Up</Button>
+        <Button variant="outline" 
+            height='28px' 
+            display={{
+                base: "none",
+                sm:"flex"
+            }}
+            width={{
+                base:'70px',
+                md:"110px",
+            
+            }}
+            mr={2}
+            onClick={() => setAuthModalState({ open: true, view: "login"})}
+        >Log In</Button>
+        <Button height="28px"
+            display={{
+                base: "none",
+                sm: "flex",
+            }}
+            width={{
+                base:"70px",
+                md:"110px"
+            }}
+            mr={2}
+            onClick={() => setAuthModalState({ open: true, view: "signup" })}
+        >Sign Up</Button>
         </>
     )
 }
