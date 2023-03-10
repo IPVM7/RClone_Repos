4e13f17a5a4e961dda980ea175628736dda58c9e
IPVM7 <ianvaldebenitom@gmail.com>
2023-03-10 20:32:13 -0300

    Added useAuthState

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Flex, Image } from "@chakra-ui/react";
 import SearchInput from './SearchInput';
 import RightContent from './RightContent/RightContent';
-
+import { auth } from '../../firebase/clientApp'
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 const Navbar:React.FC = () => {
-    
+    const [user, loading, error] = useAuthState(auth);
     return(
         <Flex bg="white" height='44px' padding='6px 12px'>
             <Flex align="center">
@@ -18,7 +19,7 @@ const Navbar:React.FC = () => {
                 />
             </Flex>
             <SearchInput/>
-            <RightContent/>
+            <RightContent user={user} />
             {/*
             <Directory/>
             */}
