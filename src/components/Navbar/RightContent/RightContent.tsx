cd859271dd5f9ed3dc15c25c3adfc55702ebf7c2
IPVM7 <ianvaldebenitom@gmail.com>
2023-03-10 20:28:55 -0300

    Added Auth User

--- src/components/Navbar/RightContent/RightContent.tsx
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -1,19 +1,23 @@
-import { Flex } from '@chakra-ui/react';
+import { Button, Flex } from '@chakra-ui/react';
+import { signOut, User } from 'firebase/auth';
 import AuthModal from 'Hypnos/components/Modal/Auth/AuthModal';
 import React from 'react';
 import AuthButtons from './AuthButtons';
-
+import { auth } from '../../../firebase/clientApp'
+import Icons from './Icons';
+import UserMenu from './UserMenu';
 type RightContetProps = {
-    //user: any;
+    user?: User | null;
 };
 
-const RightContet:React.FC<RightContetProps> = () => {
+const RightContet:React.FC<RightContetProps> = ({ user }) => {
     
     return (
         <>
         <AuthModal/>
         <Flex justify='center' align='center'>
-            <AuthButtons/>
+            {user ? <Icons/> : <AuthButtons/>}
+            <UserMenu user={user}></UserMenu>
         </Flex>   
         </>
     );
